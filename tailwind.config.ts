5362c10d6a30e45dafbbac68b6ceab9b59134445
omar-fadel <omar.fadel@vodafone.com>
2024-06-21 22:50:44 +0300

    fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,57 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "background-1": "url('/backgrounds/backgroundOne.webp')",
+      },
+      screens: {
+        sm: "599px",
+        md: "904px",
+        lg: "1440px",
+      },
+      fontFamily: {
+        sans: ["Montserrat"],
+      },
+      colors: {
+        "navy-blue": "#01121D",
+        simon: "#D22333",
+      },
+      fontSize: {
+        h1: [
+          "60px",
+          {
+            fontWeight: "bolder",
+          },
+        ],
+        h2: [
+          "32px",
+          {
+            fontWeight: "normal",
+          },
+        ],
+        h3: [
+          "24px",
+          {
+            fontWeight: "bold",
+          },
+        ],
+        body: [
+          "24px",
+          {
+            fontWeight: "normal",
+          },
+        ],
+        bodySmall: [
+          "16px",
+          {
+            fontWeight: "normal",
+          },
+        ],
+        button: [
+          "14px",
+          {
+            fontWeight: "bold",
+          },
+        ],
       },
     },
   },
